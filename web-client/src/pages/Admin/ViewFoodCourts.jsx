c14ae39df2d7e850f43e561e940b4468b00a0f72
Jayanti2919 <jayanti2919@gmail.com>
2024-09-29 11:38:54 +0530

    completed viewFoodCourts UI

--- web-client/src/pages/Admin/ViewFoodCourts.jsx
+++ web-client/src/pages/Admin/ViewFoodCourts.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import kiitlogo from "../../assets/kiitlogo.svg";
-import NavigateLinks from "../../components/NavigateLinks";
+import FoodCourtList from "../../components/FoodCourtList";
+import AdminNavigateLinks from "../../components/AdminNavigateLinks";
 
 const ViewFoodCourts = () => {
   const [foodCourts, setFoodCourts] = useState([
@@ -10,8 +11,18 @@ const ViewFoodCourts = () => {
       landmark: "MBA Garden",
       maplink: "https://google.com",
     },
+    {
+      fc_no: 10,
+      campus: 17,
+      landmark: "MBA Garden",
+      maplink: "https://google.com",
+    },
   ]);
+  
+  const [adminName, setAdminName] = useState("Admin")
   const titles = ["FC No.", "Campus", "Landmark", "Map Link", "Options"];
+
+
   return (
     <div className="bg-primary min-h-screen min-w-screen flex flex-col p-10">
       <div className="flex justify-between">
@@ -23,10 +34,10 @@ const ViewFoodCourts = () => {
             <h2 className="text-5xl text-accentgreen font-semibold">
               All Food Courts
             </h2>
-            <p className="text-accentgreen text-xl">Mr./Ms./Mrs. Admin</p>
+            <p className="text-accentgreen text-xl">Mr./Ms./Mrs. {adminName}</p>
           </div>
         </div>
-        <NavigateLinks navOption="add new FC" navLink="/addNewFoodCourt" />
+        <AdminNavigateLinks navOption1="add new FC" navLink1="/addNewFoodCourt" navOption2="add new item" navLink2="/addNewItem"/>
       </div>
       <div className="bg-secondary text-accentwhite cabin mt-6 rounded-md py-5">
         <div className="grid grid-cols-5 text-center">
@@ -36,6 +47,11 @@ const ViewFoodCourts = () => {
         </div>
         <div className="px-8">
           <div className="bg-accentwhite w-full h-1 mt-4 rounded-md"></div>
+        </div>
+        <div className="mt-10 text-center">
+          {foodCourts.map((foodCourt, idx) => (
+            <FoodCourtList key={idx} foodCourt={foodCourt} />
+          ))}
         </div>
       </div>
     </div>
